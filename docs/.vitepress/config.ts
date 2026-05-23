8291f49d59b71f5f89e0b0bfd7436617e801c635
qakosal <akos.albel@ericsson.com>
2026-05-23 11:45:18 +0200

    Add Important Notes wiki page

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -31,6 +31,7 @@ export default defineConfig({
         {
           text: 'abtars',
           items: [
+            { text: 'Important Notes', link: '/abtars/important' },
             { text: 'What is abTARS?', link: '/abtars/' },
             { text: 'Why abTARS?', link: '/abtars/why' },
             { text: 'Architecture', link: '/abtars/architecture' },
